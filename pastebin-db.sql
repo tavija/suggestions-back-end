68474d79b386cd9454a66f6d162dd7b7b2d2577e
Tavija N <tavijaja@gmail.com>
2021-09-02 15:58:20 +0300

    updated sql query to match column names

--- pastebin-db.sql
+++ pastebin-db.sql
@@ -1,6 +1,6 @@
 CREATE TABLE pastes (
     paste_id SERIAL PRIMARY KEY,
     title VARCHAR (300),
-    paste_text TEXT NOT NULL,
+    content TEXT NOT NULL,
     time TIMESTAMP NOT NULL default now()
 )
